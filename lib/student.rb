49dc7f6a7458c409fd7b2d27fd33cba79d78249e
Sierra Wallace <sskwallace@gmail.com>
2020-12-24 00:50:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,11 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER,
     )
+    SQL 
 end
 
   # Remember, you can access your database connection anywhere in this class
