34127f50de726d84f38d08d01e75ee564c081040
Sierra Wallace <sskwallace@gmail.com>
2020-12-24 00:57:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,7 @@ class Student
     SQL
  
     DB[:conn].execute(sql, self.name, self.grade)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
