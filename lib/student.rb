4ae38ec95d84b1a4a3f828742293ffa89669970f
Sierra Wallace <sskwallace@gmail.com>
2020-12-24 00:47:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,7 @@
 class Student
-
+  attr_accessor :name, :grade
+  attr_reader :id 
+  def initialize(name, grade,id=nil)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
